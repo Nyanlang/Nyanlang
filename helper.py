473b00675ea57e9474db5046003d8e49c9c6aa57
sserve-kr <personal@sserve.me>
2023-02-28 19:01:31 +0900

    improved code quality and help messages

---
+++ helper.py
@@ -0,0 +1,28 @@
+_base = f"Usage: python3.11 {__file__}"
+
+
+class ParamItem:
+    def __init__(self, name: str, desc: str):
+        self.name = name
+        self.desc = desc
+
+
+class Param:
+    def __init__(self, command_name: str, description_name: str, *items: ParamItem, no_desc: bool = False):
+        self.command_name = command_name
+        self.description_name = description_name
+        self.items: tuple[ParamItem] = items
+        self.no = no_desc
+
+
+def help_generator(child: str, *params: Param) -> str:
+    param_help = ""
+
+    for param in params:
+        if param.no:
+            continue
+        param_help += "\n\n"
+        param_help += f"{param.description_name}:\n"
+        param_help += "\n".join(["  "+f"{item.name} - {item.desc}" for item in param.items])
+
+    return _base + f" {child} {' '.join([f'<{pk.command_name}>' for pk in params])}" + param_help
--- interpreter.py
+++ interpreter.py
@@ -1,71 +1,100 @@
 import sys
 import os
 import re
+from helper import Param, ParamItem
+from helper import help_generator as helper
 
-if len(sys.argv) == 1:
-    raise ValueError("Usage: python interpreter.py <filename>")
+HELP = {
+    "_": helper(
+        "",
+        Param(
+            "command",
+            "Commands",
+            ParamItem("run", "Run a file")
+        )
+    ),
+    "run": helper(
+        "run",
+        Param("filename", "", no_desc=True)
+    )
+}
 
-filename = sys.argv[1]
-
-if not os.path.exists(filename):
-    raise FileNotFoundError(f"File {filename} not found")
-
-with open(filename, "r") as f:
-    program = re.sub(r'".*?"', "", f.read().replace("\n", "").replace(" ", "") + " ")
-
-jump_points = {}
-next_points = {}
+RUN_HELP = f"Usage: python3.11 {__file__} run <filename>"
 
 
-def find_matchpoint(start_pair):
-    next_to = -1
-    for i in range(start_pair+1, len(program)):
-        if i <= next_to:
-            continue
-        if program[i] == "-":
-            jump_points[i] = start_pair
-            next_points[start_pair] = i
-            return i
+def return_(v, e=1):
+    print(v)
+    exit(e)
+
+
+def run(filename):
+    if not os.path.exists(filename):
+        raise FileNotFoundError(f"File {filename} not found")
+
+    with open(filename, "r") as f:
+        program = re.sub(r'".*?"', "", f.read().replace("\n", "").replace(" ", "") + " ")
+
+    jump_points = {}
+    next_points = {}
+
+    def find_matchpoint(start_pair):
+        next_to = -1
+        for i in range(start_pair+1, len(program)):
+            if i <= next_to:
+                continue
+            if program[i] == "-":
+                jump_points[i] = start_pair
+                next_points[start_pair] = i
+                return i
+            if program[i] == "~":
+                next_to = find_matchpoint(i)
+        raise SyntaxError("No matching - for ~")
+
+    for i in range(len(program)):
         if program[i] == "~":
-            next_to = find_matchpoint(i)
-    raise SyntaxError("No matching - for ~")
+            find_matchpoint(i)
+
+    pointer = 0
+    memory = {}
+    cursor = 0
+
+    while cursor < len(program):
+        char = program[cursor]
+        if char == " ":
+            break
+        if char not in "?!냥냐.,~-뀨":
+            raise ValueError(f"Invalid character {char}")
+        match char:
+            case "?":
+                pointer += 1
+            case "!":
+                pointer -= 1
+            case "냥":
+                memory[pointer] = memory.get(pointer, 0) + 1
+            case "냐":
+                memory[pointer] = memory.get(pointer, 0) - 1
+            case ".":
+                print(chr(memory.get(pointer, 0)), end="")
+            case ",":
+                memory[pointer] = ord((lambda ip: ip[0] if len(ip) >= 1 else 0)(input()))
+            case "~":
+                if memory.get(pointer, 0) == 0:
+                    cursor = next_points[cursor]
+            case "-":
+                if memory.get(pointer, 0) != 0:
+                    cursor = jump_points[cursor]
+            case "뀨":
+                print(memory.get(pointer, 0))
+
+        cursor += 1
 
 
-for i in range(len(program)):
-    if program[i] == "~":
-        find_matchpoint(i)
-
-
-pointer = 0
-memory = {}
-cursor = 0
-
-while cursor < len(program):
-    char = program[cursor]
-    if char == " ":
-        break
-    if char not in "?!냥냐.,~-뀨":
-        raise ValueError(f"Invalid character {char}")
-    match char:
-        case "?":
-            pointer += 1
-        case "!":
-            pointer -= 1
-        case "냥":
-            memory[pointer] = memory.get(pointer, 0) + 1
-        case "냐":
-            memory[pointer] = memory.get(pointer, 0) - 1
-        case ".":
-            print(chr(memory.get(pointer, 0)), end="")
-        case ",":
-            memory[pointer] = ord((lambda ip: ip[0] if len(ip) >= 1 else 0)(input()))
-        case "~":
-            if memory.get(pointer, 0) == 0:
-                cursor = next_points[cursor]
-        case "-":
-            if memory.get(pointer, 0) != 0:
-                cursor = jump_points[cursor]
-        case "뀨":
-            print(memory.get(pointer, 0))
-
-    cursor += 1
+match sys.argv:
+    case [_]:
+        return_(HELP["_"])
+    case [_, "run"]:
+        return_(HELP["run"])
+    case [_, "run", *f]:
+        run(' '.join(f))
+    case _:
+        raise ValueError(f"Invalid command {sys.argv}")
