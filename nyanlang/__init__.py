bc57464e9c40659931df8fbf26b7a133ffe96c76
sserve-kr <personal@sserve.me>
2023-03-29 21:49:51 +0900

    Move translate to external package

--- nyanlang/__init__.py
+++ nyanlang/__init__.py
@@ -21,29 +21,13 @@ HELP = {
             "command",
             "Commands",
             ParamItem("run", "Run a file"),
-            ParamItem("translate", "Translate a file to nyanlang language from other language"),
-            ParamItem("visual", "Visualize nyanlang memory")
         )
     ),
     "run": helpgen.help(
         "run",
         Param("filename", "", no_desc=True),
         Param("debug", "", no_desc=True, optional=True, kw="d")
-    ),
-    "translate": helpgen.help(
-        "translate",
-        Param(
-            "language",
-            "Languages",
-            ParamItem("bf", "Brainfuck"),
-        ),
-        Param("filename", "", no_desc=True),
-        Param("dest", "", no_desc=True, optional=True, kw="d")
-    ),
-}
-
-LANG = {
-    "bf": ["b", "bf"]
+    )
 }
 
 
@@ -58,37 +42,9 @@ def main():
             if "-d" in options or "--debug" in options:
                 debug = True
             NyanEngine(f, debug=debug).run()
-        case [_, "translate"]:
-            return_(HELP["translate"])
-        case [_, "translate", _]:
-            return_(HELP["translate"])
-        case [_, "translate", language, f, *options]:
-            _dest = None
-            if "-d" in options:
-                _dest = pathlib.Path(options[options.index("-d") + 1])
-            if "--dest" in options:
-                _dest = pathlib.Path(options[options.index("--dest") + 1])
-
-            if language not in LANG:
-                raise ValueError(f"Invalid language {language}")
-
-            if f.split(".")[-1] not in LANG[language]:
-                raise ValueError(f"Invalid file extension .{' '.join(f).split('.')[-1]} - "
-                                 f"File extension must be {'or'.join(['.'+i for i in LANG[language]])}")
-
-            if not pathlib.Path(f).exists():
-                raise FileNotFoundError(f"File {f} not found")
-
-            if not _dest:
-                _dest = pathlib.Path('.'.join(f.split(".")[:-1] + ["nyan"]))
-
-            if _dest.exists():
-                raise FileExistsError(f"File {_dest} already exists")
-
-            translate(language, f, _dest)
         case cmd:
             try:
-                __import__("nyan-ext-"+cmd[1]).run()
+                __import__("nyan_ext_"+cmd[1]).run()
             except ModuleNotFoundError:
                 return_(f"Invalid command \"{sys.argv[1]}\"")
 
